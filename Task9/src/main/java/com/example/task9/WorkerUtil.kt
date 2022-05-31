b722f93b55514c20693ad0ed306b26c4469768a4
AkakiGit <77205622+AkakiGit@users.noreply.github.com>
2022-05-31 15:48:14 +0400

    task9

---
+++ Task9/src/main/java/com/example/task9/WorkerUtil.kt
@@ -0,0 +1,13 @@
+package com.example.task9
+
+import android.util.Log
+
+fun doSomethingWorkerOne(name: String) {
+
+    Log.i("Worker One", name)
+}
+
+fun doSomethingWorkerTwo(name: String) {
+
+    Log.i("Worker Two", name)
+}
